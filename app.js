6be1dd0af90067bf48f8880bb1aff443f79b51f1
Benjh4 <benjamin.plaza@alumnos.ucn.cl>
2023-11-28 21:18:33 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from "cors"
+import "dotenv/config"
+
+const app = express()
+
+app.use(cors())
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
